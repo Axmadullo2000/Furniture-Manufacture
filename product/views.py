95bdca8ceb201ee9f5c1a84f026550e86053f4a8
Axmadullo <axmadullo2000@gmail.com>
2021-12-15 23:09:13 +0500

    Changes to deploy

--- product/views.py
+++ product/views.py
@@ -40,10 +40,9 @@ def product_detail(request, category_slug, product_slug):
     single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
     products = Product.objects.annotate(similarity=TrigramSimilarity('product_name',
                                                                      single_product.product_name))
-    single = Product.objects.get(category__slug=category_slug, slug=product_slug)
     in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     context = {
-        'single_product': single,
+        'single_product': single_product,
         'products': products,
         'in_cart': in_cart,
     }
